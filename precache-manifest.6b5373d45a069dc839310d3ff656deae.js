01d20dacb626e24bade55e5911fe20dbe9798c5e
menaveille <contactvarka@gmail.com>
2020-10-27 00:00:16 +0100

    Updates

---
+++ precache-manifest.6b5373d45a069dc839310d3ff656deae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b26bef46404da268ddcb56c9221ab0",
+    "url": "/golf/index.html"
+  },
+  {
+    "revision": "17c1c2530ce293af8d9c",
+    "url": "/golf/static/css/main.c75447ba.chunk.css"
+  },
+  {
+    "revision": "145ab1da013a672fc645",
+    "url": "/golf/static/js/2.aa6482dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/golf/static/js/2.aa6482dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c1c2530ce293af8d9c",
+    "url": "/golf/static/js/main.21f8bb23.chunk.js"
+  },
+  {
+    "revision": "587691aff8329b2151c1",
+    "url": "/golf/static/js/runtime-main.81cf42d5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/golf/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/golf/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d642a768586d4efcb269ddfb9047a0c1",
+    "url": "/golf/static/media/gift.d642a768.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/golf/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/golf/static/media/manOnTable.52be3144.svg"
+  }
+]);
